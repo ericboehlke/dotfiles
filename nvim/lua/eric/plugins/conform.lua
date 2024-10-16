ef8b5837b90f00118c986223d5bdd362126e7d82
Eric Boehlke <eric.p.boehlke@gmail.com>
2024-10-16 11:45:43 -0500

    Fix conform

--- nvim/lua/eric/plugins/conform.lua
+++ nvim/lua/eric/plugins/conform.lua
@@ -1,40 +1,43 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "black" },
-				json = { "jq" },
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>fm",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- This will provide type hinting with LuaLS
+	---@module "conform"
+	---@type conform.setupOpts
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black" },
+			json = { "jq" },
+		},
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500 },
+		-- Customize formatters
+		formatters = {
+			black = {
+				prepend_args = { "--line-length", "100" },
 			},
-			-- Use this to debug issues.
-			-- log_level = vim.log.levels.DEBUG,
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				async = false,
-				timeout_ms = 1000,
-				lsp_fallback = true,
-			},
-			formatters = {
-				black = {
-					prepend_args = {
-						{ "--line-length", "100" },
-					},
-				},
-			},
-		})
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
-	opts = {},
 }
